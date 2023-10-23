b16438fbf28acdf7391e85e865a192ad97495452
Evgeny Golovin <yokopoomy20@gmail.com>
2023-10-23 20:49:49 +0300

    Fix eslint

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -2,92 +2,92 @@ import * as characters from '../character';
 
 test('create Bowerman', () => {
   const bowerman = new characters.Bowerman('Char1');
-	
+
   expect(bowerman).toEqual(
-		{
-			"name": "Char1",
-			"health": 100,
-			"level": 1,
-			"type": "Bowerman",
-			"attack": 25,
-			"defence": 25
-		}
-	);
+    {
+      name: 'Char1',
+      health: 100,
+      level: 1,
+      type: 'Bowerman',
+      attack: 25,
+      defence: 25,
+    },
+  );
 });
 
 test('create Swordsman', () => {
-	const swordsman = new characters.Swordsman('Char2');
-	
+  const swordsman = new characters.Swordsman('Char2');
+
   expect(swordsman).toEqual(
-		{
-			"name": "Char2",
-			"health": 100,
-			"level": 1,
-			"type": "Swordsman",
-			"attack": 40,
-			"defence": 10
-		}
-	);
+    {
+      name: 'Char2',
+      health: 100,
+      level: 1,
+      type: 'Swordsman',
+      attack: 40,
+      defence: 10,
+    },
+  );
 });
 
 test('create Magician', () => {
-	const magician = new characters.Magician('Char3');
-	
+  const magician = new characters.Magician('Char3');
+
   expect(magician).toEqual(
-		{
-			"name": "Char3",
-			"health": 100,
-			"level": 1,
-			"type": "Magician",
-			"attack": 10,
-			"defence": 40
-		}
-	);
+    {
+      name: 'Char3',
+      health: 100,
+      level: 1,
+      type: 'Magician',
+      attack: 10,
+      defence: 40,
+    },
+  );
 });
 
 test('create Undead', () => {
-	const undead = new characters.Undead('Char4');
-	
+  const undead = new characters.Undead('Char4');
+
   expect(undead).toEqual(
-		{
-			"name": "Char4",
-			"health": 100,
-			"level": 1,
-			"type": "Undead",
-			"attack": 25,
-			"defence": 25
-		}
-	);
+    {
+      name: 'Char4',
+      health: 100,
+      level: 1,
+      type: 'Undead',
+      attack: 25,
+      defence: 25,
+    },
+  );
 });
 
 test('create Zombie', () => {
-	const zombie = new characters.Zombie('Char5');
-	
+  const zombie = new characters.Zombie('Char5');
+
   expect(zombie).toEqual(
-		{
-			"name": "Char5",
-			"health": 100,
-			"level": 1,
-			"type": "Zombie",
-			"attack": 40,
-			"defence": 10
-		}
-	);
+    {
+      name: 'Char5',
+      health: 100,
+      level: 1,
+      type: 'Zombie',
+      attack: 40,
+      defence: 10,
+    },
+  );
 });
 
 test('create Daemon', () => {
-	const daemon  = new characters.Daemon ('Char6');
-	
+  const daemon = new characters.Daemon('Char6');
+
   expect(daemon).toEqual(
-		{
-			"name": "Char6",
-			"health": 100,
-			"level": 1,
-			"type": "Daemon",
-			"attack": 10,
-			"defence": 40
-		}
-	);
+    {
+      name: 'Char6',
+      health: 100,
+      level: 1,
+      type: 'Daemon',
+      attack: 10,
+      defence: 40,
+    },
+  );
 });
 
 test('check long name', () => {
@@ -108,55 +108,55 @@ test('check short name', () => {
 
 test('check name string', () => {
   const char = new characters.Bowerman('CharName');
-  expect(typeof char.name).toBe('string')
+  expect(typeof char.name).toBe('string');
 });
 
 test('take damage', () => {
-	const bowerman = new characters.Bowerman('Char1');
-	bowerman.damage(100);
+  const bowerman = new characters.Bowerman('Char1');
+  bowerman.damage(100);
 
-	expect(bowerman).toEqual(
-		{
-			"name": "Char1",
-			"health": 25,
-			"level": 1,
-			"type": "Bowerman",
-			"attack": 25,
-			"defence": 25
-		}
-	)
-})
+  expect(bowerman).toEqual(
+    {
+      name: 'Char1',
+      health: 25,
+      level: 1,
+      type: 'Bowerman',
+      attack: 25,
+      defence: 25,
+    },
+  );
+});
 
 test('levelUp', () => {
-	const bowerman = new characters.Bowerman('Char1');
-	bowerman.levelUp();
+  const bowerman = new characters.Bowerman('Char1');
+  bowerman.levelUp();
 
-	expect(bowerman).toEqual(
-		{
-			"name": "Char1",
-			"health": 100,
-			"level": 2,
-			"type": "Bowerman",
-			"attack": 30,
-			"defence": 30
-		}
-	)
-})
+  expect(bowerman).toEqual(
+    {
+      name: 'Char1',
+      health: 100,
+      level: 2,
+      type: 'Bowerman',
+      attack: 30,
+      defence: 30,
+    },
+  );
+});
 
 test('LevelUp < 0 health', () => {
-	const bowerman = new characters.Bowerman('Char1');
-	bowerman.damage(150);
+  const bowerman = new characters.Bowerman('Char1');
+  bowerman.damage(150);
 
-	expect(() => {
-		bowerman.levelUp()
-	}).toThrow();
-})
+  expect(() => {
+    bowerman.levelUp();
+  }).toThrow();
+});
 
 test('Damage < 0 health', () => {
-	const bowerman = new characters.Bowerman('Char1');
-	bowerman.damage(150);
+  const bowerman = new characters.Bowerman('Char1');
+  bowerman.damage(150);
 
-	expect(() => {
-		bowerman.damage(10);
-	}).toThrow();
-})
+  expect(() => {
+    bowerman.damage(10);
+  }).toThrow();
+});
--- src/js/character.js
+++ src/js/character.js
@@ -1,11 +1,11 @@
 class Character {
-	constructor(name) {
-		this.setName(name);
-		this.health = 100;
-		this.level = 1;
-	}
+  constructor(name) {
+    this.setName(name);
+    this.health = 100;
+    this.level = 1;
+  }
 
-	setName(name) {
+  setName(name) {
     if (name.length <= 10 && name.length >= 2 && typeof name === 'string') {
       this.name = name;
     } else {
@@ -13,76 +13,76 @@ class Character {
     }
   }
 
-	levelUp() {
-		this.level = this.level + 1;
-		this.attack += this.attack * 0.2;
-		this.defence += this.defence * 0.2;
-		if (this.health <= 0) {
-			throw new Error('The character is dead')
-		} else {
-			this.health = 100;
-		}
-	}
+  levelUp() {
+    this.level += 1;
+    this.attack += this.attack * 0.2;
+    this.defence += this.defence * 0.2;
+    if (this.health <= 0) {
+      throw new Error('The character is dead');
+    } else {
+      this.health = 100;
+    }
+  }
 
-	damage(points) {
-		if (this.health >= 0) {
-			this.health -= points * (1 - this.defence / 100);
-		} else {
-			throw new Error('The character is dead');
-		}
-	}
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    } else {
+      throw new Error('The character is dead');
+    }
+  }
 }
 
 export class Bowerman extends Character {
-	constructor(name, health, level, levelUp, damage) {
-		super(name, health, level, levelUp, damage);
-		this.type = 'Bowerman';
-		this.attack = 25;
-		this.defence = 25;
-	}
+  constructor(name, health, level, levelUp, damage) {
+    super(name, health, level, levelUp, damage);
+    this.type = 'Bowerman';
+    this.attack = 25;
+    this.defence = 25;
+  }
 }
 
 export class Swordsman extends Character {
-	constructor(name, health, level) {
-		super(name, health, level);
-		this.type = 'Swordsman';
-		this.attack = 40;
-		this.defence = 10;
-	}
+  constructor(name, health, level) {
+    super(name, health, level);
+    this.type = 'Swordsman';
+    this.attack = 40;
+    this.defence = 10;
+  }
 }
 
 export class Magician extends Character {
-	constructor(name, health, level) {
-		super(name, health, level);
-		this.type = 'Magician';
-		this.attack = 10;
-		this.defence = 40;
-	}
+  constructor(name, health, level) {
+    super(name, health, level);
+    this.type = 'Magician';
+    this.attack = 10;
+    this.defence = 40;
+  }
 }
 
 export class Undead extends Character {
-	constructor(name, health, level) {
-		super(name, health, level);
-		this.type = 'Undead';
-		this.attack = 25;
-		this.defence = 25;
-	}
+  constructor(name, health, level) {
+    super(name, health, level);
+    this.type = 'Undead';
+    this.attack = 25;
+    this.defence = 25;
+  }
 }
 
 export class Zombie extends Character {
-	constructor(name, health, level) {
-		super(name, health, level);
-		this.type = 'Zombie';
-		this.attack = 40;
-		this.defence = 10;
-	}
+  constructor(name, health, level) {
+    super(name, health, level);
+    this.type = 'Zombie';
+    this.attack = 40;
+    this.defence = 10;
+  }
 }
 
 export class Daemon extends Character {
-	constructor(name, health, level) {
-		super(name, health, level);
-		this.type = 'Daemon';
-		this.attack = 10;
-		this.defence = 40;
-	}
+  constructor(name, health, level) {
+    super(name, health, level);
+    this.type = 'Daemon';
+    this.attack = 10;
+    this.defence = 40;
+  }
 }
